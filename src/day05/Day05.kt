1b0ca9d901f23953c9642fc2da4799f3de09d128
Edson Arratea Ope <edson.arratea.93@gmail.com>
2023-12-04 21:44:20 -0500

    chore: Add Day5 config

---
+++ src/day05/Day05.kt
@@ -0,0 +1,27 @@
+package day05
+
+import println
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput1 = readInput("day05","Day05_test")
+    check(part1(testInput1) == 0)
+
+    val testInput2 = readInput("day05","Day05_2_test")
+    check(part2(testInput2) == 0)
+
+    val input1 = readInput("day05", "Day05")
+    part1(input1).println()
+
+    val input2 = readInput("day05", "Day05_2")
+    part2(input2).println()
+}
